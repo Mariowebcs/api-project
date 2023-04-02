40f526ff76d30cff0d500d4b56dda689383fdf5a
Mariowebcs <114620811+Mariowebcs@users.noreply.github.com>
2023-04-02 23:31:19 +0200

    Split up the server to make it testable

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,7 @@
-import express from "express";
-import "express-async-errors";
-
-const app = express();
-
-app.get("/", (request, response) => {
-    response.send("This is the space fact API!");
-});
+import app from "./app";
 
 const port = 3000;
 
 app.listen(port, () => {
     console.log(`[Server] : Server is running at : http://localhost:${port}`);
 });
-
