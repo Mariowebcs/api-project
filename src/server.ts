77cd5a3136445b80ed3f4452ba90d0b50b1f165e
Mariowebcs <114620811+Mariowebcs@users.noreply.github.com>
2023-04-02 23:23:41 +0200

    configured automatic build and restart

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import "express-async-errors";
 const app = express();
 
 app.get("/", (request, response) => {
-    response.send("up and running");
+    response.send("This is the space fact API!");
 });
 
 const port = 3000;
